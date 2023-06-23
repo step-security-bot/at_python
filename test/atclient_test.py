c762f7139f0596c13ae63588f9f59c3b98de1897
umangshah <shahumang19@gmail.com>
2023-06-23 11:30:01 -0400

    chore: atsign customization for tests using config.ini file

--- test/atclient_test.py
+++ test/atclient_test.py
@@ -1,52 +1,63 @@
 import unittest
+from configparser import ConfigParser
 
 from src.common import AtSign, AtClient
 from src.common.keys import PublicKey, SelfKey, SharedKey, PrivateHiddenKey
 
 class AtClientTest(unittest.TestCase):
     verbose = False
+    atsign1 = ""
+    atsign2 = ""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        config = ConfigParser()
+        config.read('config.ini')
+        cls.atsign1 = config.get("test_atsigns", "atsign1", fallback="@27barracuda")
+        cls.atsign2 = config.get("test_atsigns", "atsign2", fallback="@amateur93")
+        return super().setUpClass()
 
     def test_atsign_pkam_authenticate(self):
         """Test PKAM Authentication"""
-        atsign = AtSign("27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         self.assertTrue(atclient.is_authenticated())
 
     def test_get_at_keys(self):
         """Test Scan Verb using get_at_keys"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         my_keys = atclient.get_at_keys("", fetch_metadata=True)
         self.assertTrue(len(my_keys)>0)
 
     def test_put_public_key(self):
         """Test Put Function with Public Key"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         pk = PublicKey("test_public_key", atsign)
         response = atclient.put(pk, "test1")
         self.assertIsNotNone(response)
 
     def test_put_self_key(self):
         """Test Put Function with Self Key"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         sk = SelfKey("test_self_key", atsign)
         response = atclient.put(sk, "test1")
         self.assertIsNotNone(response)
 
     def test_put_shared_key(self):
         """Test Put Function with Shared Key"""
-        shared_by = AtSign("@27barracuda")
-        shared_with = AtSign("@amateur93")
-        atclient = AtClient(shared_by, verbose=AtClientTest.verbose)
+        shared_by = AtSign(self.atsign1)
+        shared_with = AtSign(self.atsign2)
+        atclient = AtClient(shared_by, verbose=self.verbose)
         sk = SharedKey("test_shared_key", shared_by, shared_with)
         response = atclient.put(sk, "test1")
         self.assertIsNotNone(response)
 
-        shared_with = AtSign("@27barracuda")
-        shared_by = AtSign("@amateur93")
-        atclient = AtClient(shared_by, verbose=AtClientTest.verbose)
+        shared_with = AtSign(self.atsign1)
+        shared_by = AtSign(self.atsign2)
+        atclient = AtClient(shared_by, verbose=self.verbose)
         sk = SharedKey("test_shared_key2", shared_by, shared_with)
         response = atclient.put(sk, "test2")
         self.assertIsNotNone(response)
@@ -54,31 +65,31 @@ class AtClientTest(unittest.TestCase):
 
     def test_get_self_key(self):
         """Test Get Function with Self Key"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         sk = SelfKey("test_self_key", atsign)
         response = atclient.get(sk)
         self.assertEqual("test1", response)
 
     def test_get_public_key(self):
         """Test Get Function with Public Key"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         pk = PublicKey("test_public_key", atsign)
         response = atclient.get(pk)
         self.assertEqual("test1", response)
 
-        amateur_atsign = AtSign("@amateur93")
-        atclient = AtClient(amateur_atsign, verbose=AtClientTest.verbose)
+        amateur_atsign = AtSign(self.atsign2)
+        atclient = AtClient(amateur_atsign, verbose=self.verbose)
         response = atclient.get(pk)
         self.assertEqual("test1", response)
 
     def test_get_shared_key(self):
         """Test Get Function with Shared Key"""
         # Shared by me with other
-        shared_by = AtSign("@27barracuda")
-        shared_with = AtSign("@amateur93")
-        atclient = AtClient(shared_by, verbose=AtClientTest.verbose)
+        shared_by = AtSign(self.atsign1)
+        shared_with = AtSign(self.atsign2)
+        atclient = AtClient(shared_by, verbose=self.verbose)
         sk = SharedKey("test_shared_key1", shared_by, shared_with)
         response = atclient.get(sk)
         self.assertEqual("test1", response)
@@ -90,8 +101,8 @@ class AtClientTest(unittest.TestCase):
 
     def test_delete_public_key(self):
         """Test Delete Function with Public Key"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         pk = PublicKey("delete_test", atsign)
         response = atclient.put(pk, "test1")
 
@@ -100,8 +111,8 @@ class AtClientTest(unittest.TestCase):
 
     def test_delete_self_key(self):
         """Test Delete Function with Self Key"""
-        atsign = AtSign("@27barracuda")
-        atclient = AtClient(atsign, verbose=AtClientTest.verbose)
+        atsign = AtSign(self.atsign1)
+        atclient = AtClient(atsign, verbose=self.verbose)
         sk = SelfKey("delete_self_key_test", atsign)
         response = atclient.put(sk, "test1")
 
@@ -110,9 +121,9 @@ class AtClientTest(unittest.TestCase):
 
     def test_delete_shared_key(self):
         """Test Delete Function with Shared Key"""
-        shared_by = AtSign("@27barracuda")
-        shared_with = AtSign("@amateur93")
-        atclient = AtClient(shared_by, verbose=AtClientTest.verbose)
+        shared_by = AtSign(self.atsign1)
+        shared_with = AtSign(self.atsign2)
+        atclient = AtClient(shared_by, verbose=self.verbose)
         sk = SharedKey("delete_shared_key_test", shared_by, shared_with)
         response = atclient.put(sk, "test1")
 
